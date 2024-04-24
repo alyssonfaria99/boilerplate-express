2a5c6c180544ad01ee81e975572fadaf7b80c9a0
Alysson Faria <alyssonfaria99@gmail.com>
2024-04-24 10:42:44 -0300

    dd

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let app = express();
 
 console.log('Hello World');
 
+app.use('/public', (req, res) => {
+    express.static(__dirname + '/public;')
+})
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 })
