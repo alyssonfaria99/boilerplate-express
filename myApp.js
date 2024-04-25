da4e3eadbf2ce33a6afc4c6784b52c1790fa308a
Alysson Faria <alyssonfaria99@gmail.com>
2024-04-25 09:43:15 -0300

    ee

--- myApp.js
+++ myApp.js
@@ -32,6 +32,10 @@ app.get('/now', (req, res, next) => {
     next();
 }, (req, res) => {
     res.json({ time: req.time });
+});
+
+app.get('/:word/echo', (req, res) => {
+    res.json({ echo: req.params.word });
 })
 
 
