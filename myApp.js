01e6a49c42342838fc561d4b4f560dcd08ac893d
Alysson Faria <alyssonfaria99@gmail.com>
2024-04-25 10:13:48 -0300

    yy

--- myApp.js
+++ myApp.js
@@ -44,6 +44,8 @@ app.route('/name').get((req, res) => {
     let lastname = req.query.last
     res.json({ name: firstname + ' ' + lastname });
 }).post((req, res) => {
+    let firstname = req.query.first
+    let lastname = req.query.last
     res.json({ name: firstname + ' ' + lastname });
 })
 
